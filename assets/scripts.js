80e2e732c4385e1247393721b6e0d2ec4d27ebc9
miintto <minjp93@gmail.com>
2024-08-31 01:52:12 +0900

    feat: 네비게이션바 개선

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,12 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+document.getElementById('toggleButton').addEventListener('click', function() {
+  const content = document.getElementById('navbarResponsive');
+  const isExpanded = this.getAttribute('aria-expanded') === 'true';
+  
+  if (isExpanded) {
+    content.style.display = 'none';
+    this.setAttribute('aria-expanded', 'false');
+  } else {
+    content.style.display = 'block';
+    this.setAttribute('aria-expanded', 'true');
+  }
+});
