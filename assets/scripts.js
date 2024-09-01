a31c4815ba21159adb1463c4249454e849abb6bf
miintto <minjp93@gmail.com>
2024-09-02 08:07:14 +0900

    feat: 네비게이션바 클릭시 transition 추가

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,10 +3,14 @@ document.getElementById('toggleButton').addEventListener('click', function() {
   const isExpanded = this.getAttribute('aria-expanded') === 'true';
   
   if (isExpanded) {
-    content.style.display = 'none';
+    content.style.height = '0px';
     this.setAttribute('aria-expanded', 'false');
   } else {
-    content.style.display = 'block';
+    var height = 10;
+    content.querySelectorAll('.nav-item').forEach(element => {
+      height += element.offsetHeight;
+    });
+    content.style.height = height + 'px';
     this.setAttribute('aria-expanded', 'true');
   }
 });
